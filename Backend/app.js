9ac425404217f3290d2a00b093891f8458b96911
stef <stef.m@hotmail.be>
2023-11-22 11:01:13 +0100

    Added port to .env

--- Backend/app.js
+++ Backend/app.js
@@ -63,7 +63,9 @@ app.use((err, req, res, next) => {
   res.status(500).send("Something broke!");
 });
 
-// PORT
-app.listen(8000, () => {
-  console.log("Server is running on post 8000");
+// START SERVER
+const PORT = process.env.PORT || 8080;
+
+app.listen(PORT, () => {
+  console.log("Server is running on port " + PORT);
 });
